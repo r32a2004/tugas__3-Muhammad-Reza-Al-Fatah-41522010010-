510ccd01d2abdf9b8911303b3f6deae43b1a9014
muhammad reza al fatah <muhammadrezaalfatah09@gmail.com>
2024-04-16 21:33:05 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas_3/register.java
@@ -0,0 +1,31 @@
+package com.example.tugas_3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void register (View view) {
+        Intent intent = new Intent(register.this, login.class);
+        startActivity(intent );
+
+    }
+}
